5997f15e04371e9bc23f35c2a292ce4a899755cf
rahuln17b <rahuln17b@gmail.com>
2024-11-24 14:20:06 +0530

    union sample

---
+++ sop.py
@@ -0,0 +1,9 @@
+set1 = {'A','B','C','D','E'}
+set2 = {'B','D','V','X','Y','Z'}
+
+union = set1.union(set2)
+
+total = list(union)
+
+print("Total guests enjoyin will be:",len(total))
+print("Guest list:",total)
